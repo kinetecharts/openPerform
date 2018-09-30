3c098e44459e253924bb39cc5e8e4670eb2c701a
jerknose <travis@travisbennett.com>
2018-09-29 17:12:58 -0700

    Updated example config.

--- server/config_example.js
+++ server/config_example.js
@@ -46,7 +46,7 @@ const config = {
     },
   },
   perceptionNeuron: {
-    enabled: true,
+    enabled: false,
     ip: '192.168.1.250', // ip the Axis Neuron translation app is broadcasting from
     ports: {
       incoming: 9000, // port the Axis Neuron translation app is broadcasting on
@@ -54,36 +54,42 @@ const config = {
     },
   },
   gamepads: {
-    enabled: true,
+    enabled: false,
     ports: {
       outgoing: 9101, // port the browser connects on
     },
   },
   midiController: {
-    enabled: true,
+    enabled: false,
     ports: {
       outgoing: 9301, // port the browser connects on
     },
   },
   oscController: {
-    enabled: true,
+    enabled: false,
     ports: {
       incoming: 9400,
       outgoing: 9401, // port the browser connects on
     },
   },
   poseNet: {
-    enabled: true,
+    enabled: false,
     ports: {
       incoming: 9500,
       outgoing: 9501, // port the browser connects on
     },
   },
   iPhoneX: {
-    enabled: true,
+    enabled: false,
     ports: {
       incoming: 9600,
       outgoing: 9601, // port the browser connects on
+    },
+  },
+  kinectron: {
+    enabled: true,
+    ports: {
+      incoming: 9700,
     },
   },
 };
