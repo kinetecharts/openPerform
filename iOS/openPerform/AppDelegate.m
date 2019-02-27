c4ac1c0b9bd2e027d4a772abb04c8241146d12f1
Travis Bennett <travis@travisbennett.com>
2019-02-26 21:16:34 -0800

    New process for building and compiling a full version (server and all!) for iOS.

---
+++ iOS/openPerform/AppDelegate.m
@@ -0,0 +1,72 @@
+//
+//  AppDelegate.m
+//  native-xcode-node-folder
+//
+//  Created by Jaime Bernardo on 08/03/2018.
+//  Copyright © 2018 Janea Systems. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "NodeRunner.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)startNode {
+    NSString* srcPath = [[NSBundle mainBundle] pathForResource:@"nodejs-project/server/fileServer.js" ofType:@""];
+    NSArray* nodeArguments = [NSArray arrayWithObjects:
+                                @"node",
+                                srcPath,
+                                nil
+                                ];
+    [NodeRunner startEngineWithArguments:nodeArguments];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSThread* nodejsThread = nil;
+    nodejsThread = [[NSThread alloc]
+        initWithTarget:self
+        selector:@selector(startNode)
+        object:nil
+    ];
+    // Set 2MB of stack space for the Node.js thread.
+    [nodejsThread setStackSize:2*1024*1024];
+    [nodejsThread start];
+
+    [self.window setFrame:[[UIScreen mainScreen] bounds]];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+@end
---
+++ webpack-ios.config.js
@@ -0,0 +1,101 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebappWebpackPlugin = require('webapp-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = require('./server/config');
+
+module.exports = {
+  mode: 'production',
+  context: resolve(__dirname, 'src'),
+  entry: [
+    './index.jsx',
+    // the entry point of our app
+  ],
+  output: {
+    // the output bundle
+    filename: 'bundle.js',
+
+    publicPath: './',
+
+    path: resolve(__dirname, 'app/iOS_basic/nodejs-project/dist/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?name=/fonts/[name].[ext]',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader?name=/fonts/[name].[ext]',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+      }, {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
+      {
+        test: /\.(jpe?g|gif|svg)$/i,
+        use: [
+          'img-loader?name=images/[name].[ext]',
+          'url-loader?name=images/[name].[ext]&limit=10000',
+        ],
+      },
+      {
+        test: /\.(png)$/i,
+        use: [
+          'base64-image-loader',
+        ],
+      },
+      {
+        test: /\.glsl$/,
+        loader: 'webpack-glsl-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: resolve(__dirname, './dist/'),
+        to: resolve(__dirname, 'app/iOS_basic/nodejs-project/dist/'),
+      },
+      {
+        from: resolve(__dirname, './server/'),
+        to: resolve(__dirname, 'app/iOS_basic/nodejs-project/server/'),
+      },
+    ]),
+    new WebappWebpackPlugin(resolve(__dirname, './src/static/images/op_logo.png')),
+    new HtmlWebpackPlugin({
+      title: 'Open Perform by Kinetech Arts',
+      template: resolve(__dirname, './src/html/index.html'),
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      THREE: 'three',
+      'window.THREE': 'three',
+      TWEEN: 'tween.js',
+      'window.TWEEN': 'tween.js',
+      React: 'react',
+      _: 'lodash',
+    }),
+  ],
+};
